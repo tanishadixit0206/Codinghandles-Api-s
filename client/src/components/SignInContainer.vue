4cbc5ca6eb9234966a64594377353c78c5ed8279
dixittanisha02 <dixittanisha02@gmail.com>
2024-05-26 22:14:41 +0530

    feat writes google auth script

--- client/src/components/SignInContainer.vue
+++ client/src/components/SignInContainer.vue
@@ -1,11 +1,49 @@
 <script lang="ts">
+import { ref } from "vue";
+import axios from "axios";
+
+const accessToken = ref<string | null>(null);
+
+const signInWithGoogle = () => {
+  const clientId = "";
+  const redirectUri = "";
+  const googleAuthUrl = "";
+  window.location.href = googleAuthUrl;
+};
+
+const getAccessToken = async (code: string) => {
+  const clientId = "";
+  const clientSecret = "";
+  const redirectUri = "";
+
+  try {
+    const response = await axios.post("", {
+      code,
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: redirectUri,
+      grant_type: "authorization_code",
+    });
+    accessToken.value = response.data.access_token;
+  } catch (error) {
+    console.error("Failed to exchange code for access token", error);
+  }
+};
+
+const urlParams = new URLSearchParams(window.location.search);
+const code = urlParams.get("code");
+
+if (code) {
+  getAccessToken(code);
+}
+
 export default {
   name: "SignInContainer",
 };
 </script>
 
 <template>
-  <div >
+  <div>
     <h1>Sign In</h1>
     <button href="">
       <img src="" />
