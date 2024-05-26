4e7a7721b044b51ca42ef918e77e6fc119dc47ee
dixittanisha02 <dixittanisha02@gmail.com>
2024-05-26 08:48:50 +0530

    feat writes basic script and template structure for AccessTokenPage

---
+++ client/src/views/AccessTokenPage.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import LogoItem from "../components/LogoItem.vue";
+import AccessTokenContainer from "../components/AccessTokenContainer.vue";
+export default defineComponent({
+  name: "LandingPage",
+  components: { LogoItem, AccessTokenContainer },
+});
+</script>
+
+<template>
+  <div>
+    <LogoItem />
+    <div>
+      <AccessTokenContainer />
+    </div>
+  </div>
+</template>
