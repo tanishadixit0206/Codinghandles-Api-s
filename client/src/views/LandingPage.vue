7a2902aed058b8dc6dad472ef03613df167113c5
dixittanisha02 <dixittanisha02@gmail.com>
2024-05-26 08:49:04 +0530

    feat writes basic script and template structure for LandingPage

---
+++ client/src/views/LandingPage.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import LogoItem from "../components/LogoItem.vue";
+import SignInContainer from "../components/SignInContainer.vue";
+export default defineComponent({
+  name: "LandingPage",
+  components: { LogoItem, SignInContainer },
+});
+</script>
+
+<template>
+  <div>
+    <LogoItem />
+    <div>
+      <SignInContainer />
+    </div>
+  </div>
+</template>
