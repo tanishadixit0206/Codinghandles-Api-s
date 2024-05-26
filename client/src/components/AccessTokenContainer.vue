aa73477843e3db4c0fb7aa8c506b3b487edd8794
dixittanisha02 <dixittanisha02@gmail.com>
2024-05-26 08:46:24 +0530

    feat writes basic script and template structure for AccessTokenContainer and corrects the file name

---
+++ client/src/components/AccessTokenContainer.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+export default {
+  name: "AccessTokenContainer",
+};
+</script>
+
+<template>
+  <div>
+    <h1>Sign In</h1>
+    <div><img src="" />Token Recieved<img src="" /></div>
+    <p>Wow,looks like you have got a Token</p>
+    <div>
+      <!-- token -->
+    </div>
+    <div><img />Google Verification<img /></div>
+    <button>Let's go</button>
+  </div>
+</template>
+
+<style scoped></style>
