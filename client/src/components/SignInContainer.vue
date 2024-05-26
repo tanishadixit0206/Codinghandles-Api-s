885c9ed7cc222040e1b15336d7ddf52fa5cd3e10
dixittanisha02 <dixittanisha02@gmail.com>
2024-05-26 08:48:27 +0530

    feat writes basic script and template structure for SignInContainer

--- client/src/components/SignInContainer.vue
+++ client/src/components/SignInContainer.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts">
-
+<script lang="ts">
+export default {
+  name: "SignInContainer",
+};
 </script>
 
 <template>
-
+  <div >
+    <h1>Sign In</h1>
+    <button href="">
+      <img src="" />
+      Sign in with Google
+    </button>
+  </div>
 </template>
+
+<style scoped></style>
