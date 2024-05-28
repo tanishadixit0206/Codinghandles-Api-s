924847314b9b7a689528761757fdd7fe045387f0
dixittanisha02 <dixittanisha02@gmail.com>
2024-05-28 14:48:35 +0530

    feat adds SwaggerUI component

---
+++ client/src/views/SwaggerUI.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import SwaggerUI from "swagger-ui-dist/swagger-ui.es-bundle.js";
+import "swagger-ui-dist/swagger-ui.css";
+
+onMounted(() => {
+  SwaggerUI({
+    dom_id: "#swagger-ui",
+    url: "",
+  });
+});
+</script>
+
+<template>
+  <div id="swagger-ui"></div>
+</template>
+
+<style scoped></style>
